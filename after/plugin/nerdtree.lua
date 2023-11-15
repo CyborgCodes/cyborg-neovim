8537a91a8376f53ed77731c536917744579f791b
CT <changtheng2@gmail.com>
2023-11-15 15:24:17 +0800

    removed nerdTree

--- after/plugin/nerdtree.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', '<C-n>', vim.cmd.NERDTree)
-vim.keymap.set('n', '<C-t>', vim.cmd.NERDTreeToggle)
-vim.keymap.set('n', '<leader>e', vim.cmd.NERDTreeFocus)
--- plugin/lsp_zero.lua
+++ plugin/lsp_zero.lua
@@ -1,10 +1,18 @@
 local lsp_zero = require('lsp-zero')
+local dart_lsp = lsp_zero.build_options('dartls', {})
 
 lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 
   vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, {buffer = bufnr})
+
 end)
+
+require('flutter-tools').setup({
+  lsp = {
+    capabilities = dart_lsp.capabilities
+  }
+})
 
 lsp_zero.format_on_save({
   format_opts = {
@@ -14,5 +22,5 @@ lsp_zero.format_on_save({
   servers = {
     ['tsserver'] = {'javascript', 'typescript', 'tsx', 'javascriptreact', 'typescriptreact'},
     ['dartls'] = {'dart'}
-  }
+  },
 })
