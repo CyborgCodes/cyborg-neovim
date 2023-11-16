2d36e3887d97adf93900a19f8ba6ad3fee39fbe8
CT <changtheng2@gmail.com>
2023-11-16 16:20:52 +0800

    added gitsigns & autoindent

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -7,8 +7,10 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.colorcolumn = '80'
 
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.wrap = false
 
